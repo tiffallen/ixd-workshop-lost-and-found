0782bae41465e50030036954e5ef465675d1e769
Liby99 <liby99@icloud.com>
2017-04-18 23:23:06 -0700

    Done Completion Code

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,8 @@ $("footer > tab").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
     $("#" + $(this).attr("id") + "-section").addClass("active").siblings().removeClass("active");
 });
+
+$("#found-form").submit(function () {
+    postFound();
+    return false;
+});
