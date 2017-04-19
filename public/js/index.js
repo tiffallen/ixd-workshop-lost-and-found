7a71794de076f2d60adf9829638354299972f0d7
Liby99 <liby99@icloud.com>
2017-04-18 23:50:08 -0700

    Improved Structure

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,19 @@ $("#found-form").submit(function () {
     postFound();
     return false;
 });
+
+function ajax(option) {
+    option.success = function (result) {
+        var data = JSON.parse(result);
+        if (data.code == 0) {
+            option.success(data.content);
+        }
+        else {
+            alert("Error " + data.code + ": " + data.msg);
+        }
+    };
+    option.error = function () {
+        alert("Internet connection error");
+    };
+    $.ajax(option);
+}
