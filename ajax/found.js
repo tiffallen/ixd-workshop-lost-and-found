ca145bd4f73ba8c7a499befd9462df1bf07d3017
Tiffany Allen <t5allen@ucsd.edu>
2017-04-19 16:37:11 -0700

    added datae

--- ajax/found.js
+++ ajax/found.js
@@ -1,1 +1,9 @@
 /* TODO: Implement the Found AJAX Handler */
+module.exports = {
+    post: function (req, res) {
+        req.body.lat = parseFloat(req.body.lat);
+        req.body.lng = parseFloat(req.body.lng);
+        require("./data.json)").push(req.body);
+        res.success();
+    }
+}
--- ajax/lost.js
+++ ajax/lost.js
@@ -1,1 +1,7 @@
 /* TODO: Implement the lost AJAX Handler */
+module.exports = {
+    get: function (req, res){
+        var data = require("./data.json");
+        res.success(data);
+    }
+}
